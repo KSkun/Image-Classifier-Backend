0575d2adf4f7fad32c5fd8d28bc4bbfff1bd868b
KSkun <ks@ksmeow.moe>
2021-12-26 23:58:58 +0800

    chore: improve error handling

--- src/controller/user.py
+++ src/controller/user.py
@@ -1,4 +1,5 @@
 import base64
+import binascii
 from datetime import datetime, timedelta
 from http import HTTPStatus
 
@@ -12,23 +13,18 @@ from controller.response import response_error, response_success
 from model.user import *
 
 
-@user_bp.route('token', methods=['GET'])
+@user_bp.route('/token', methods=['GET'])
 def user_get_token():
     username = request.args.get('username')
     password_encoded = request.args.get('password')
     try:
-        password = base64.b64decode(password_encoded)
-    except Exception as e:
+        password = base64.b64decode(password_encoded, validate=True)
+    except binascii.Error as e:
         abort(response_error(HTTPStatus.BAD_REQUEST, e, 'invalid username or password'))
         return
 
-    try:
-        user = get_user_by_name(username)
-    except Exception as e:
-        abort(response_error(HTTPStatus.INTERNAL_SERVER_ERROR, e, 'database exception'))
-        return
-
     # check password
+    user = find_user_by_name(username)
     if not bcrypt.checkpw(password, user['password'].encode()):
         abort(response_error(HTTPStatus.BAD_REQUEST, None, 'invalid username or password'))
         return
