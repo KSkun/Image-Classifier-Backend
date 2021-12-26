e6e511c88dcc1dbd7d047c43efaf336255c3653d
KSkun <ks@ksmeow.moe>
2021-12-26 23:58:31 +0800

    fix: hidden error message in debug mode

--- src/controller/response.py
+++ src/controller/response.py
@@ -6,7 +6,7 @@ from config import C
 
 
 def response_error(status: int, error: Any, error_mask: str, data: Any = None):
-    if not C.debug:
+    if not C.debug or error is None:
         error = error_mask
     return make_response(jsonify({'success': False, 'error': str(error), 'data': data}), status)
 
